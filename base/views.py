bf0f7dc4bc3b09630e7e535c6c040c7a23e40fca
Paweł Kruszko <pawelkruszko@gmail.com>
2023-12-22 13:48:00 +0100

    user registation

--- base/views.py
+++ base/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 
@@ -16,8 +17,13 @@ from .forms import RoomForm
 
 
 def loginPage(request):
+    page = "login"
+
+    if request.user.is_authenticated:
+        return redirect("home")
+
     if request.method == "POST":
-        username = request.POST.get("username")
+        username = request.POST.get("username").lower()
         password = request.POST.get("password")
 
         try:
@@ -33,13 +39,30 @@ def loginPage(request):
         else:
             messages.error(request, "Username or password does not exist")
 
-    context = {}
+    context = {"page": page}
     return render(request, "base/login_register.html", context)
 
 
 def logoutUser(request):
     logout(request)
     return redirect("home")
+
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "An error occurred during registation")
+
+    return render(request, "base/login_register.html", {"form": form})
 
 
 def home(request):
@@ -62,10 +85,6 @@ def room(request, pk):
 @login_required(login_url="login")
 def createRoom(request):
     form = RoomForm()
-
-    if request.user != room.host:
-        return HttpResponse("You are not allowed here!")
-
     if request.method == "POST":
         form = RoomForm(request.POST)
         if form.is_valid():
